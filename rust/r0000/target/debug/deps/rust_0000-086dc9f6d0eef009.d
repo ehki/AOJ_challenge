1f3d1305e8be00b56b3824e882d83ecb26ff4844
ehki <0y35.ejiri.vmqewyhw@gmail.com>
2018-08-10 15:57:23 +0900

    solve aoj 0000 using rust

---
+++ rust/r0000/target/debug/deps/rust_0000-086dc9f6d0eef009.d
@@ -0,0 +1,5 @@
+/Users/myb/Documents/PracticeProgramming/AOJ_challenge/rust/r0000/target/debug/deps/rust_0000-086dc9f6d0eef009: src/main.rs
+
+/Users/myb/Documents/PracticeProgramming/AOJ_challenge/rust/r0000/target/debug/deps/rust_0000-086dc9f6d0eef009.d: src/main.rs
+
+src/main.rs:
---
+++ rust/r0000/target/debug/rust_0000.d
@@ -0,0 +1,1 @@
+/Users/myb/Documents/PracticeProgramming/AOJ_challenge/rust/r0000/target/debug/rust_0000.dSYM: /Users/myb/Documents/PracticeProgramming/AOJ_challenge/rust/r0000/src/main.rs
