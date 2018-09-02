db2467398639dcf7aab1218d15c6698aa5846cdc
ehki <0y35.ejiri.vmqewyhw@gmail.com>
2018-09-02 23:15:13 +0900

    slightly speed up 0223 using python

--- python/0223.py
+++ python/0223.py
@@ -1,29 +1,27 @@
 # from line_profiler import LineProfiler
 from collections import deque
+from sys import stdin
 
 def main():
     while(True):
-        W, H = map(int, input().split())
+        W, H = map(int, stdin.readline().split())
         if not W: break
-        tx,ty = map(int, input().split())
-        kx,ky = map(int, input().split())
-        ma = [[False]*(W+2) for _ in range(H+2)]
-        for i in range(1,H+1):
-            for j,a in enumerate(input().split()):
-                ma[i][j+1] = bool(1-int(a))
-        from pprint import pprint
+        tx,ty = map(int, stdin.readline().split())
+        kx,ky = map(int, stdin.readline().split())
+        ma = [[False]+[bool(1-int(a)) for a in stdin.readline().split()]+[False] for i in range(H)]
+        ma = [[False]*(W+2)] + ma + [[False]*(W+2)]
         que = deque([[tx,ty,kx,ky,0]])
         pas = set()
         ans = "NA"
         while(que):
             tx,ty,kx,ky,c = que.popleft()
-            if c > 100: break
             if tx == kx and ty == ky:
                 ans = c
                 break
+            if c+1 > 100: continue
             for dx,dy in [[-1,0],[0,-1],[1,0],[0,1]]:
-                tdx, tdy = [ dx, dy] if ma[ty+dy][tx+dx] else [0,0]
-                kdx, kdy = [-dx,-dy] if ma[ky-dy][kx-dx] else [0,0]
+                tdx, tdy = ( dx, dy) if ma[ty+dy][tx+dx] else (0,0)
+                kdx, kdy = (-dx,-dy) if ma[ky-dy][kx-dx] else (0,0)
                 if (tx+tdx,ty+tdy,kx+kdx,ky+kdy) in pas: continue
                 que.append([tx+tdx,ty+tdy,kx+kdx,ky+kdy,c+1])
                 pas.add((tx+tdx,ty+tdy,kx+kdx,ky+kdy))
