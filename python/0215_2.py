5c14d1a7a944adcad5bbbeaba96abe3569de8a36
ehki <0y35.ejiri.vmqewyhw@gmail.com>
2018-09-05 01:09:32 +0900

    solve aoj 0215 using python

--- python/0215_2.py
+++ python/0215_2.py
@@ -1,6 +1,6 @@
 from random import randrange
-for i in range(5):
-    H,W = randrange(5,200),randrange(5,200)
+for i in range(30):
+    H,W = randrange(5,300),randrange(5,300)
     strs = [[".........................12345"[randrange(30)] for k in range(W)] for j in range(H)]
     strs[randrange(H)][randrange(W)] = "S"
     strs[randrange(H)][randrange(W)] = "G"
